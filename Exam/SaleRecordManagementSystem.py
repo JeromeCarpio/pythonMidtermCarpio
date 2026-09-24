62927f9fb4fc3be2005ef7c4062bcb4f2ce5d285
JeromeCarpio <jeromecarpio1103@gmail.com>
2026-09-24 20:24:11 +0800

    Add files via upload

---
+++ Exam/SaleRecordManagementSystem.py
@@ -0,0 +1,10 @@
+
+print("========================================")
+print(" SALES RECORD MANAGEMENT SYSTEM")
+print("========================================")
+print("1. Add Sale Record")
+print("2. View All Records & Summary Statistics")
+print("3. Clear All Sales Data")
+print("4. Exit System")
+print("========================================")
+
